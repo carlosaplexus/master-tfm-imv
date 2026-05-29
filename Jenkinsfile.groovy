8adae64b01a414ccedca8e0c7c630467336618a4
Carlos Alonso <carlosa.plexus@hotmail.com>
2026-05-29 23:56:29 +0200

    Eliminar archivos con pruebas preliminares

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,130 @@
+pipeline {
+
+    agent { 
+        label 'docker' 
+    }
+
+    stages {
+
+        stage('Unit Tests') {
+            steps {
+                sh 'make test-unit'
+                archiveArtifacts artifacts: "results/*.xml"
+            }
+        }
+
+        stage('API Tests') {
+            steps {
+                sh 'make test-api'
+                archiveArtifacts artifacts: "results/*.xml"
+            }
+        }
+
+        stage('E2E Tests') {
+            steps {
+                sh 'make test-e2e'
+                archiveArtifacts artifacts: "results/*.xml"
+            }
+        }
+
+        // stage('Debug workspace') {
+        //     steps {
+        //         sh "ls -R ${WORKSPACE}"
+        //     }
+        // }
+
+        stage('Convertir XML a HTML') {
+            steps {
+                sh '''
+                    apk add --no-cache libxslt
+                    mkdir -p results/e2e
+                    xsltproc test/e2e/junit-to-html.xsl results/cypress_result.xml > results/e2e/index.html
+                '''
+            }
+        }
+
+        stage('Publicar Reportes') {
+            steps {
+
+                junit 'results/*_result.xml' 
+
+                publishHTML(target: [
+                    reportDir: 'results/unit',
+                    reportFiles: 'index.html',
+                    reportName: 'Unit Tests Report'
+                ])
+
+                publishHTML(target: [
+                    reportDir: 'results/api',
+                    reportFiles: 'index.html',
+                    reportName: 'API Tests Report'
+                ])
+
+                publishHTML(target: [
+                    reportDir: 'results/coverage',
+                    reportFiles: 'index.html',
+                    reportName: 'Cobertura de Código'
+                ])
+
+                publishHTML(target: [
+                    reportDir: 'results/e2e',
+                    reportFiles: 'index.html',
+                    reportName: 'Reporte E2E'
+                ])
+            }
+        }
+
+        stage('Build Docker Images') {
+            steps {
+                sh 'make build'
+            }
+        }
+
+        stage('Push Docker Images') {
+            steps {
+                sh 'make push'
+            }
+        }
+
+        stage('Autenticacion en AWS Academy') {
+            steps {
+                sh 'make aws-login'
+            }
+        }
+
+        stage('Aplicar manifiestos Kubernetes') {
+            steps {
+                sh 'make deploy'
+            }
+        }    
+
+        stage('Obtener url pública aplicación') {
+            steps {
+                sh 'make deploy'
+            }
+        }       
+
+    }
+
+    post {
+        failure {
+
+            echo "Job: ${JOB_NAME}"
+            echo "Ejecución: #${BUILD_NUMBER}"
+            echo "URL: ${BUILD_URL}"           
+    //         mail to: 'correo@ejemplo.com',
+    //             subject: "Fallo en el job ${JOB_NAME} #${BUILD_NUMBER}",
+    //             body: """El pipeline ha fallado.
+
+    //         Job: ${JOB_NAME}
+    //         Ejecución: #${BUILD_NUMBER}
+    //         URL: ${BUILD_URL}
+
+    //         Revise los logs para más detalles.
+
+    //         Notificación automática desdeJenkins
+    //         """
+        }
+    }
+}
+
--- Readme_Make.md
+++ Readme_Make.md
@@ -1,38 +1,47 @@
-🧠 Cómo usar este Makefile
++ Preparacion del entorno
+-----------------------------------------
+1. Logarse en Github
+2. Logarse en Dockerhub
+3. Logarse en AWS Academy
+
+
++ Ejecutores
+-----------------------------------------
 1. Ejecutar todos los tests
-make tests
+        make tests
 
 2. Construir imágenes
-make build
+        make build
 
-3. Subir imágenes a GHCR
-make push
+3. Subir imágenes a Github
+        make push
 
 4. Autenticarse en AWS Academy
-make aws-login
+        make aws-login
 
 5. Desplegar todo en EKS
-make deploy
+        make deploy
 
 6. Obtener la URL del frontend
+        make url
 
-make url
 7. Ejecutarlo TODO de una vez
-make all
+        make all
 
-🚀 Flujo completo recomendado
-make tests
-make build
-make push
-make aws-login
-make deploy
-make url
-Abrir la URL y hacer un simulacro real
++ Flujo completo
+-----------------------------------------
+        make tests
+        make build
+        make push
+        make aws-login
+        make deploy
+        make url
 
-🧪 Cómo ejecutar los tests
-🟦 Unitarios
-make test-unit
-🟧 API
-make test-api
-🟩 Cypress (E2E)
-make test-e2e
++ Test específicos
+------------------------------------------
+- Unitarios
+    make test-unit
+- API
+    make test-api
+- Cypress (E2E)
+    make test-e2e
